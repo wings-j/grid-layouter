96ac228290c5ec294de899a389f2d375981798d6
WingsJ <mail@wings-j.cn>
2023-01-14 22:53:19 +0800

    update

--- src/app.vue
+++ src/app.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
-  import Grid from '@/core/grid'
-  import { ElMessage, ElTooltip } from 'element-plus'
+  import Grid, { Cell } from '@/core/grid'
+  import { ElInput, ElMessage, ElTooltip } from 'element-plus'
   import { reactive } from 'vue'
 
-  const grid = reactive(new Grid())
-  grid.addDimension('row', 'fr', 1)
-  grid.addDimension('row', 'fr', 1)
-  grid.addDimension('column', 'fr', 1)
-  grid.addDimension('column', 'fr', 1)
+  const grid = reactive(new Grid(['1fr', '1fr'], ['1fr', '1fr']))
+
+  /**
+   * 处理单元格点击
+   * @param cell 单元格
+   */
+  function handleCellClick(cell: Cell) {}
 
   /**
    * 处理输出点击
@@ -19,22 +21,56 @@
 </script>
 
 <template>
-  <div class="d-flex h-100vh column-gap-10" style="background-color: #f9f9f9">
-    <div class="flex-grow-1 h-100 p-20 bg-white">
+  <div class="d-grid h-100vh column-gap-10" style="grid-template-columns: 1fr 60px; background-color: #f9f9f9">
+    <div class="d-grid h-100 p-20 bg-white" style="grid-template-columns: 200px 1fr; grid-template-rows: 60px 1fr">
+      <div></div>
+      <div class="d-grid justify-items-center align-items-center" :style="{ gridTemplateColumns: grid.columns.join(' ') }">
+        <div v-for="(a, i) of grid.columns" :key="i">
+          <div class="input">
+            <div class="f-bold">C-{{ i + 1 }}</div>
+            <div class="m-l-5" style="width: 80px"><el-input v-model="grid.columns[i]" /></div>
+            <img class="m-l-15 cursor-pointer" style="height: 20px" src="./images/cross.svg" @click="grid.removeDimension('c', i)" />
+          </div>
+        </div>
+      </div>
       <div
-        style="display: grid; grid-auto-flow: row dense; width: 100%; height: 100%"
+        class="d-grid justify-items-center align-items-center"
         :style="{
-          gridTemplateColumns: `100px ${grid.columns.map(a => a.token).join(' ')}`,
-          gridTemplateRows: `100px ${grid.rows.map(a => a.token).join(' ')}`
+          gridTemplateRows: grid.rows.join(' ')
         }"
       >
-        <div style="grid-area: 1/1/2/2"></div>
-        <div v-for="(a, i) of grid.columns" :style="{ gridArea: `1 / ${i + 2} / 2 / ${i + 3}` }"></div>
-        <div v-for="(a, i) of grid.rows" :style="{ gridArea: `${i + 2} / 1 / ${i + 3} / 2` }"></div>
-        <div v-for="i of grid.rows.length * grid.columns.length" :key="i" class="item">{{ i }}</div>
+        <div v-for="(a, i) of grid.rows" :key="i">
+          <div class="input">
+            <div class="f-bold">R-{{ i + 1 }}</div>
+            <div class="m-l-5" style="width: 80px"><el-input v-model="grid.rows[i]" /></div>
+            <img class="m-l-15 cursor-pointer" style="height: 20px" src="./images/cross.svg" @click="grid.removeDimension('r', i)" />
+          </div>
+        </div>
+      </div>
+      <div>
+        <div
+          class="d-grid w-100 h-100"
+          :style="{
+            gridTemplateColumns: grid.columns.join(' '),
+            gridTemplateRows: grid.rows.join(' ')
+          }"
+        >
+          <div
+            v-for="a of grid.cells"
+            :key="a.index"
+            class="d-flex flex-column justify-content-center align-items-center cursor-pointer"
+            style="color: hsl(160.55, 98.8%, 43.03%); border: 2px dashed currentColor"
+            :style="a.style"
+          >
+            <div style="font-size: 40px">{{ a.index + 1 }}</div>
+            <div class="m-t-15 f-20 t-pre" style="color: lightgray">{{ `R-${a.rowStart}:${a.rowEnd}   C-${a.columnStart}:${a.columnEnd}` }}</div>
+          </div>
+        </div>
       </div>
     </div>
     <div class="d-flex flex-column align-items-center h-100 p-v-20 bg-white" style="width: 60px">
+      <div class="f-28 cursor-pointer" @click="grid.addDimension('r', '1fr')">+R</div>
+      <div class="m-t-25 f-28 cursor-pointer" @click="grid.addDimension('c', '1fr')">+C</div>
       <el-tooltip effect="dark" content="Copy CSS to clipboard" placement="top-start">
         <img class="m-t-auto cursor-pointer" style="width: 50%" src="./images/sharp.svg" @click="handleOutputClick" />
       </el-tooltip>
@@ -43,12 +79,11 @@
 </template>
 
 <style scoped lang="scss">
-  .item {
-    display: flex;
-    justify-content: center;
+  .input {
+    display: inline-flex;
     align-items: center;
-    border: 2px dashed currentColor;
-    color: hsl(160.55, 98.8%, 43.03%);
-    font-size: 40px;
+    padding: 0 10px;
+    border-left: 2px solid #eee;
+    border-right: 2px solid #eee;
   }
 </style>
--- src/core/grid.ts
+++ src/core/grid.ts
@@ -2,64 +2,113 @@
  * 网格
  */
 class Grid {
-  rows: Dimension[] = []
-  columns: Dimension[] = []
+  rows: string[] = []
+  columns: string[] = []
+  cells: Cell[] = []
 
+  get style() {
+    return `grid-template-rows: ${this.rows.join(' ')}; grid-template-columns: ${this.columns.join(' ')};`
+  }
   get css() {
-    return `.container {
-  display: grid;
-  grid-template-rows: ${this.rows.map(a => a.token).join(' ')};
-  grid-template-columns: ${this.columns.map(a => a.token).join(' ')};
-}`
+    return `.grid { display: grid; ${this.style} }`
+  }
+
+  /**
+   * 构造方法
+   * @param rows 行
+   * @param columns 列
+   */
+  constructor(rows: string[], columns: string[]) {
+    this.rows = rows
+    this.columns = columns
+
+    let index = 0
+    for (let i = 0; i < this.rows.length; i++) {
+      for (let j = 0; j < this.columns.length; j++) {
+        this.cells.push(new Cell(index++, i + 1, i + 2, j + 1, j + 2))
+      }
+    }
   }
 
   /**
    * 添加
    * @param dimension 维度
-   * @param type 类型
    * @param value 值
    */
-  addDimension(dimension: GridDimension, type: DimensionType, value?: number) {
-    let d = new Dimension(type, value)
-    if (dimension == 'row') {
-      this.rows.push(d)
-    } else if (dimension == 'column') {
-      this.columns.push(d)
+  addDimension(dimension: GridDimension, value: string) {
+    if (dimension == 'r') {
+      this.rows.push(value)
+    } else if (dimension == 'c') {
+      this.columns.push(value)
     }
+
+    this.arrange()
   }
+  /**
+   * 删除
+   * @param dimension 维度
+   * @param index 索引
+   */
+  removeDimension(dimension: GridDimension, index: number) {
+    if (dimension == 'r') {
+      this.rows.splice(index, 1)
+    } else if (dimension == 'c') {
+      this.columns.splice(index, 1)
+    }
+
+    this.arrange()
+  }
+  /**
+   * 合并单元格
+   * @param cells 单元格
+   */
+  combineCells(cells: Cell[]) {}
+  /**
+   * 合并单元格
+   * @param cell 单元格
+   */
+  separateCells(cell: Cell) {}
+
+  /**
+   * 安排
+   */
+  private arrange() {}
 }
 
-type GridDimension = 'row' | 'column'
+type GridDimension = 'r' | 'c'
 
 /**
- * 维度
+ * 单元
  */
-class Dimension {
-  type: DimensionType
-  value?: number
+class Cell {
+  index: number
+  rowStart: number
+  rowEnd: number
+  columnStart: number
+  columnEnd: number
 
-  get token() {
-    switch (this.type) {
-      case 'px':
-        return `${this.value}px`
-      case 'fr':
-        return `${this.value}fr`
-      default:
-        return 'auto'
-    }
+  get style() {
+    return `grid-area: ${this.rowStart} / ${this.columnStart} / ${this.rowEnd} / ${this.columnEnd}`
+  }
+  get css() {
+    return `.cell-${this.index} { ${this.style} }`
   }
 
   /**
    * 构造方法
-   * @param type 类型
-   * @param value 值
+   * @param index 索引
+   * @param rowStart 行起始
+   * @param rowEnd 行结束
+   * @param columnStart 列起始
+   * @param columnEnd 列结束
    */
-  constructor(type: DimensionType, value?: number) {
-    this.type = type
-    this.value = value
+  constructor(index: number, rowStart: number, rowEnd: number, columnStart: number, columnEnd: number) {
+    this.index = index
+    this.rowStart = rowStart
+    this.rowEnd = rowEnd
+    this.columnStart = columnStart
+    this.columnEnd = columnEnd
   }
 }
 
-type DimensionType = 'fr' | 'px' | 'auto'
-
-export { Grid as default, Dimension }
+export { Grid as default, Cell }
